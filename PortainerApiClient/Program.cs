3446aca64b1858269cb985db0fa84652a740502e
Bo Christian Skjøtt <bcs@zylinc.com>
2020-07-27 16:08:49 +0200

    Updated

--- PortainerApiClient/Program.cs
+++ PortainerApiClient/Program.cs
@@ -1,11 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using PortainerApiClient.Models;
-using System.Linq;
 
 namespace PortainerApiClient
 {
@@ -57,21 +57,42 @@ namespace PortainerApiClient
 //            Console.WriteLine($"JSON response: {jsonOutput}");
 
             int index = jsonOutput.IndexOf("\"Id\":");
-            string id = jsonOutput.Substring(index + 5);
-            id = id.Substring(0, id.IndexOf(','));
-            Console.WriteLine($"Endpoint ID: {id}");
+            string dockerEndpointId = jsonOutput.Substring(index + 5);
+            dockerEndpointId = dockerEndpointId.Substring(0, dockerEndpointId.IndexOf(','));
+            Console.WriteLine($"Endpoint ID: {dockerEndpointId}");
 
-            // List containers
-            response = httpClient.GetAsync($"api/endpoints/{id}/docker/containers/json").Result;
+            var containers = ListContainers(httpClient, dockerEndpointId);
+
+            // Kill the 'calculator-api' container
+            Console.WriteLine("\nKilling the calculator-api container");
+            string containerId = containers.Where(x => x.Image.Equals("calculator-api")).Select(x => x.Id).FirstOrDefault();
+
+            response = httpClient.PostAsync($"api/endpoints/{dockerEndpointId}/docker/containers/{containerId}/kill", null).Result;
             if (!response.IsSuccessStatusCode)
             {
-                Console.WriteLine($"Container list request failed. Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
+                Console.WriteLine($"Kill container request failed. Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
                 return;
             }
 
-            jsonOutput = response.Content.ReadAsStringAsync().Result;
-//            Console.WriteLine($"JSON response: {jsonOutput}");
+            Thread.Sleep(100);
 
+            while (true)
+            {
+                ListContainers(httpClient, dockerEndpointId);
+                Thread.Sleep(5000);
+            }
+        }
+
+        private static IEnumerable<Container> ListContainers(HttpClient httpClient, string endpointId)
+        {
+            HttpResponseMessage response = httpClient.GetAsync($"api/endpoints/{endpointId}/docker/containers/json").Result;
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"Container list request failed. Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
+                return null;
+            }
+
+            string jsonOutput = response.Content.ReadAsStringAsync().Result;
             var containers = JsonSerializer.Deserialize<List<Container>>(jsonOutput);
 
             Console.WriteLine("\nContainers:");
@@ -86,43 +107,7 @@ namespace PortainerApiClient
                 Console.WriteLine("--------------");
             }
 
-            // Kill a container
-            Console.WriteLine("\nKilling the calculator-api container");
-            string containerId = containers.Where(x => x.Image.Equals("calculator-api")).Select(x => x.Id).FirstOrDefault();
-
-            response = httpClient.PostAsync($"api/endpoints/{id}/docker/containers/{containerId}/kill", null).Result;
-            if (!response.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"Kill container request failed. Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
-                return;
-            }
-
-            Thread.Sleep(100);
-
-            // List containers
-            response = httpClient.GetAsync($"api/endpoints/{id}/docker/containers/json").Result;
-            if (!response.IsSuccessStatusCode)
-            {
-                Console.WriteLine($"Container list request failed. Status code: {response.StatusCode}, Reason: {response.ReasonPhrase}");
-                return;
-            }
-
-            jsonOutput = response.Content.ReadAsStringAsync().Result;
-            containers = JsonSerializer.Deserialize<List<Container>>(jsonOutput);
-
-            Console.WriteLine("\nContainers:");
-            Console.WriteLine("--------------");
-
-            foreach (var container in containers)
-            {
-                Console.WriteLine($"ID: {container.Id}");
-                Console.WriteLine($"Image: {container.Image}");
-                Console.WriteLine($"State: {container.State}");
-                Console.WriteLine($"Status: {container.Status}");
-                Console.WriteLine("--------------");
-            }
-
-            httpClient.Dispose();
+            return containers;
         }
     }
 }
